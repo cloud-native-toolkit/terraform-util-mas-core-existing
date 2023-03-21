6095421335d4c50f1f499f7c031e8a8210c507e5
Sean Sundberg <seansund@us.ibm.com>
2023-03-21 13:02:20 -0500

    Initial version (#1)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,20 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+
+output "core_namespace" {
+  description = "The namespace where the module will be deployed"
+  value       = var.core_namespace
+}
+
+output "entitlement_secret_name" {
+  description = "The name of the secret that contains the entitlement_key"
+  value       = var.entitlement_secret_name
+}
+
+output "mas_instance_id" {
+  description = "The identifier of the MAS instance that was created"
+  value       = var.mas_instance_id
+}
+
+output "mas_workspace_id" {
+  description = "The identifier of the MAS workspace that was created"
+  value       = var.mas_workspace_id
+}
